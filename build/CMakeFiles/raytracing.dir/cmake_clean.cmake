50d27a70b5996bf24ebd34e212e007e9a5e260b9
ckakbulut <cankeremakbulut@gmail.com>
2024-04-27 14:54:42 +0300

    created project structure

---
+++ build/CMakeFiles/raytracing.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracing.dir/raytracing.cpp.o"
+  "CMakeFiles/raytracing.dir/raytracing.cpp.o.d"
+  "libraytracing.a"
+  "libraytracing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/raytracing.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include "raytracing.hpp"
+
+std::string my_lib_function(){
+  return "In library";
+}
