1f6cdeff8e97dde5121150edf27dcab94228bf9d
ckakbulut <cankeremakbulut@gmail.com>
2024-04-28 22:05:46 +0300

    added color utility header file

---
+++ include/color.h
@@ -0,0 +1,22 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <iostream>
+
+#include "vec3.h"
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Write the translated [0,255] value of each color component.
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
+
