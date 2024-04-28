ae8202d3ca17a8607a4d99b1e4362e7b5e326c70
ckakbulut <cankeremakbulut@gmail.com>
2024-04-28 22:29:55 +0300

    changed name to CamelCase and removed color alias

--- include/vec3.h
+++ include/vec3.h
@@ -4,12 +4,12 @@
 #include <cmath>
 #include <iostream>
 
-class vec3 {
+class Vec3 {
     public:
     double e[3];
 
-    vec3() : e{0,0,0} {}
-    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+    Vec3() : e{0,0,0} {}
+    Vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
     double x() const {
         return e[0];
@@ -23,8 +23,8 @@ class vec3 {
         return e[2];
     }
     
-    vec3 operator-() const {
-        return vec3( -e[0], -e[1], -e[2] );
+    Vec3 operator-() const {
+        return Vec3( -e[0], -e[1], -e[2] );
     }
     
     // return a copy of the element at that position
@@ -37,21 +37,21 @@ class vec3 {
         return e[i];
     }
 
-    vec3& operator+=(const vec3& v) {
+    Vec3& operator+=(const Vec3& v) {
         e[0] += v.e[0];
         e[1] += v.e[1];
         e[2] += v.e[2];
         return *this;
     }
 
-    vec3& operator*=(double t){
+    Vec3& operator*=(double t){
         e[0] *= t;
         e[1] *= t;
         e[2] *= t;
         return *this;
     }
 
-    vec3& operator/=(double t) {
+    Vec3& operator/=(double t) {
         return *this *= 1/t;
     }
 
@@ -66,46 +66,44 @@ class vec3 {
 
 };
 
-// Type aliases for vec3
+// Type aliases for Vec3
+using point = Vec3; // 3D point, useful for geometric clarity
 
-using point3 = vec3; // 3D point, useful for geometric clarity
-using color = vec3; // RGB color, useful when dealing with color related operations
-
-// vec3 Utility Functions
-inline std::ostream& operator<<(std::ostream& out, const vec3& v){
+// Vec3 Utility Functions
+inline std::ostream& operator<<(std::ostream& out, const Vec3& v){
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
-inline vec3 operator+(const vec3& u, const vec3& v){
-    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+inline Vec3 operator+(const Vec3& u, const Vec3& v){
+    return Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
-inline vec3 operator-(const vec3& u, const vec3& v){
-    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+inline Vec3 operator-(const Vec3& u, const Vec3& v){
+    return Vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
-inline vec3 operator*(const vec3& u, const vec3& v){
-    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+inline Vec3 operator*(const Vec3& u, const Vec3& v){
+    return Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3& v){
-    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+inline Vec3 operator*(double t, const Vec3& v){
+    return Vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator*(const vec3& v, double t){
+inline Vec3 operator*(const Vec3& v, double t){
     return t * v;
 }
 
-inline vec3 operator/ (const vec3& v, double t){
+inline Vec3 operator/ (const Vec3& v, double t){
     return (1/t) * v;
 }
 
-inline double dot(const vec3& u, const vec3& v) {
+inline double dot(const Vec3& u, const Vec3& v) {
     return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
 }
 
-inline vec3 cross(const vec3& u, const vec3& v){
-    return vec3( u.e[1] * v.e[2] - u.e[2] * v.e[1], u.e[2] * v.e[0] - u.e[0] * v.e[2], u.e[0] * v.e[1] - u.e[1] * v.e[0] );
+inline Vec3 cross(const Vec3& u, const Vec3& v){
+    return Vec3( u.e[1] * v.e[2] - u.e[2] * v.e[1], u.e[2] * v.e[0] - u.e[0] * v.e[2], u.e[0] * v.e[1] - u.e[1] * v.e[0] );
 }
 
 #endif
