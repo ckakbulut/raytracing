d5a0229666cf0ad52c053d658289f001b6b258eb
ckakbulut <cankeremakbulut@gmail.com>
2024-04-28 22:33:49 +0300

    added namespace wrappers for both classes

--- include/color.h
+++ include/color.h
@@ -5,20 +5,21 @@
 
 #include "vec3.h"
 
-using Color = Vec3; // RGB color, useful when dealing with color related operations
+namespace color{
+    using Color = Vec3; // RGB color, useful when dealing with color related operations
 
-void write_color(std::ostream& out, const Color& pixel_color) {
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
+    void write_color(std::ostream& out, const Color& pixel_color) {
+        auto r = pixel_color.x();
+        auto g = pixel_color.y();
+        auto b = pixel_color.z();
 
-    // Write the translated [0,255] value of each color component.
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+        // Write the translated [0,255] value of each color component.
+        int rbyte = int(255.999 * r);
+        int gbyte = int(255.999 * g);
+        int bbyte = int(255.999 * b);
 
-    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+        out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+    }
 }
-
 #endif
 
--- include/vec3.h
+++ include/vec3.h
@@ -4,106 +4,108 @@
 #include <cmath>
 #include <iostream>
 
-class Vec3 {
-    public:
-    double e[3];
+namespace vec3{
+    class Vec3 {
+        public:
+        double e[3];
 
-    Vec3() : e{0,0,0} {}
-    Vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+        Vec3() : e{0,0,0} {}
+        Vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-    double x() const {
-        return e[0];
+        double x() const {
+            return e[0];
+        }
+
+        double y() const {
+            return e[1];
+        }
+
+        double z() const {
+            return e[2];
+        }
+        
+        Vec3 operator-() const {
+            return Vec3( -e[0], -e[1], -e[2] );
+        }
+        
+        // return a copy of the element at that position
+        double operator[] (int i) const {
+            return e[i];
+        }
+
+        // return the actual element at the position
+        double& operator[] (int i) {
+            return e[i];
+        }
+
+        Vec3& operator+=(const Vec3& v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this;
+        }
+
+        Vec3& operator*=(double t){
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        Vec3& operator/=(double t) {
+            return *this *= 1/t;
+        }
+
+        double length() const{
+            return std::sqrt(length_squared());
+        }
+
+        double length_squared() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+
+
+    };
+
+    // Type aliases for Vec3
+    using point = Vec3; // 3D point, useful for geometric clarity
+
+    // Vec3 Utility Functions
+    inline std::ostream& operator<<(std::ostream& out, const Vec3& v){
+        return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
     }
 
-    double y() const {
-        return e[1];
+    inline Vec3 operator+(const Vec3& u, const Vec3& v){
+        return Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
     }
 
-    double z() const {
-        return e[2];
-    }
-    
-    Vec3 operator-() const {
-        return Vec3( -e[0], -e[1], -e[2] );
-    }
-    
-    // return a copy of the element at that position
-    double operator[] (int i) const {
-        return e[i];
+    inline Vec3 operator-(const Vec3& u, const Vec3& v){
+        return Vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
     }
 
-    // return the actual element at the position
-    double& operator[] (int i) {
-        return e[i];
+    inline Vec3 operator*(const Vec3& u, const Vec3& v){
+        return Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
     }
 
-    Vec3& operator+=(const Vec3& v) {
-        e[0] += v.e[0];
-        e[1] += v.e[1];
-        e[2] += v.e[2];
-        return *this;
+    inline Vec3 operator*(double t, const Vec3& v){
+        return Vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
     }
 
-    Vec3& operator*=(double t){
-        e[0] *= t;
-        e[1] *= t;
-        e[2] *= t;
-        return *this;
+    inline Vec3 operator*(const Vec3& v, double t){
+        return t * v;
     }
 
-    Vec3& operator/=(double t) {
-        return *this *= 1/t;
+    inline Vec3 operator/ (const Vec3& v, double t){
+        return (1/t) * v;
     }
 
-    double length() const{
-        return std::sqrt(length_squared());
+    inline double dot(const Vec3& u, const Vec3& v) {
+        return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
     }
 
-    double length_squared() const {
-        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    inline Vec3 cross(const Vec3& u, const Vec3& v){
+        return Vec3( u.e[1] * v.e[2] - u.e[2] * v.e[1], u.e[2] * v.e[0] - u.e[0] * v.e[2], u.e[0] * v.e[1] - u.e[1] * v.e[0] );
     }
-
-
-};
-
-// Type aliases for Vec3
-using point = Vec3; // 3D point, useful for geometric clarity
-
-// Vec3 Utility Functions
-inline std::ostream& operator<<(std::ostream& out, const Vec3& v){
-    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
-}
-
-inline Vec3 operator+(const Vec3& u, const Vec3& v){
-    return Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
-}
-
-inline Vec3 operator-(const Vec3& u, const Vec3& v){
-    return Vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
-}
-
-inline Vec3 operator*(const Vec3& u, const Vec3& v){
-    return Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
-}
-
-inline Vec3 operator*(double t, const Vec3& v){
-    return Vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
-}
-
-inline Vec3 operator*(const Vec3& v, double t){
-    return t * v;
-}
-
-inline Vec3 operator/ (const Vec3& v, double t){
-    return (1/t) * v;
-}
-
-inline double dot(const Vec3& u, const Vec3& v) {
-    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
-}
-
-inline Vec3 cross(const Vec3& u, const Vec3& v){
-    return Vec3( u.e[1] * v.e[2] - u.e[2] * v.e[1], u.e[2] * v.e[0] - u.e[0] * v.e[2], u.e[0] * v.e[1] - u.e[1] * v.e[0] );
 }
 
 #endif
