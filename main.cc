ef84c78dd36f494ea96018fbfbffdfc78aa1f95b
ckakbulut <cankeremakbulut@gmail.com>
2024-05-13 21:45:59 +0300

    added ray sphere intersection and raytraced it

--- main.cc
+++ main.cc
@@ -4,7 +4,37 @@
 #include "include/color.h"
 #include "include/ray.h"
 
+bool hit_sphere (const vec3::Point3& center, double radius, const ray::Ray& r) {
+
+  // calculating the ray-sphere intersection formula using the following:
+  // for a sphere centered at Cx, Cy, Cz, and a sphere x2 + y2 + z2 = r2:
+  // Vector from a point P to center of sphere is C-P
+  // (Cx - x)2 + (Cy - y)2 + (Cz - z)2 = r2
+  // which is basically the dot product squared of C-P 
+  // We want to know if a point on ray P(t) = Q + td satisfies the equation
+  // So subbing point with ray equation to solve for t
+  // we get t2d dot d - 2td dot (C - Q) + (C - Q) dot (C - Q) -r2 = 0
+  // using quadratic formula (since t2d is quadratic) and solving for t:
+  // a = d dot d 
+  // b = -2d dot (C - Q)
+  // c = (C - Q) dot (C - Q) - r2
+  // with the discriminant, we only want to return the higher value (if 2 solutions, will give us the one that hit the sphere closer to the viewport)
+  vec3::Vec3 oc = center - r.origin();
+  auto a = vec3::dot(r.direction(), r.direction());
+  auto b = -2.0* vec3::dot(r.direction(), oc);
+  auto c = vec3::dot(oc, oc ) - radius * radius;
+  auto discriminant = b*b - 4*a*c;
+  return (discriminant >= 0);
+  
+
+}
+
 color::Color ray_color(const ray::Ray& r) {
+
+  // place a sphere at 0, 0, -1 on the z-axis
+  if(hit_sphere(vec3::Point3(0, 0, -1), 0.5, r)){
+    return color::Color(1, 0, 0);
+  }
   vec3::Vec3 unit_direction = vec3::unit_vector(r.direction());
   float a = 0.5 * (unit_direction.y() + 1.0);
   // using linear interpolation (lerp) formula to get linearly blended colors on output
